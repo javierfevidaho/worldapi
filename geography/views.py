7f5643c02a5a4b6febd7c82974fe5a05817d576f
Javierelix <javierfevidaho@gmail.com>
2024-07-28 21:05:20 -0600

    Add home view and configure URL patterns

--- geography/views.py
+++ geography/views.py
@@ -1,10 +1,6 @@
 from rest_framework import generics
 from .models import Country
 from .serializers import CountrySerializer
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("Welcome to the CyberLotto Bank API!")
 
 class CountryList(generics.ListCreateAPIView):
     queryset = Country.objects.all()
--- worldapi/urls.py
+++ worldapi/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import home  # Import the home view
+from .views import home  # Importar la vista home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('geography.urls')),  # Include geography app URLs
-    path('', home, name='home'),  # Add this line to handle the root URL
+    path('api/', include('geography.urls')),  # Incluir las URLs del app geography
+    path('', home, name='home'),  # Manejar la URL raíz con la vista home
 ]
