5864de9d56cff7fd83675a66f7c6aa46ce6240cc
Javierelix <javierfevidaho@gmail.com>
2024-07-28 20:55:29 -0600

    Include geography URLs and add home view

--- worldapi/urls.py
+++ worldapi/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import home  # Import the home view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('geography.urls')),
+    path('api/', include('geography.urls')),  # Include geography app URLs
+    path('', home, name='home'),  # Add this line to handle the root URL
 ]
